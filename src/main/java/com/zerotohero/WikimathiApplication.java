5ad53cefd51165ef198caa93353f19a54b1e5470
Mehmet <mathdenizi@gmail.com>
2022-10-10 02:06:57 +0200

    Multi Module Project Created

---
+++ src/main/java/com/zerotohero/WikimathiApplication.java
@@ -0,0 +1,13 @@
+package com.zerotohero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikimathiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikimathiApplication.class, args);
+    }
+
+}
