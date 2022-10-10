e7e448632473b51f789c3606cca41ddcd0460eeb
Mehmet <mathdenizi@gmail.com>
2022-10-10 20:37:38 +0200

    Producer Module finished work properly

--- kafka-producer-wikimedia/src/main/java/com/zeroToHero/SpringBootProducerApplication.java
+++ kafka-producer-wikimedia/src/main/java/com/zeroToHero/SpringBootProducerApplication.java
@@ -1,12 +1,24 @@
 package com.zeroToHero;
 
+import com.zeroToHero.kafka.WikimedieChangesProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootProducerApplication {
+public class SpringBootProducerApplication  implements CommandLineRunner {
+    @Autowired
+    private WikimedieChangesProducer wikimedieChangesProducer;
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootProducerApplication.class);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        wikimedieChangesProducer.sendMessage();
     }
 }
