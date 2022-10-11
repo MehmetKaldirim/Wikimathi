da007ec4ee08e07e339648b9a2e1e1d7c8711d28
Mehmet <mathdenizi@gmail.com>
2022-10-11 22:52:48 +0200

    retrieve the data from producer to consumer

---
+++ kafka-consumer-database/src/main/java/com/zeroToHero/entity/WikimediaData.java
@@ -0,0 +1,22 @@
+package com.zeroToHero.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "wikimedia_changes")
+@Getter
+@Setter
+@Component
+public class WikimediaData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Lob
+    private String wikiEventData;
+
+}
--- kafka-consumer-database/src/main/java/com/zeroToHero/kafka/KafkaDatabaseConsumer.java
+++ kafka-consumer-database/src/main/java/com/zeroToHero/kafka/KafkaDatabaseConsumer.java
@@ -1,5 +1,7 @@
 package com.zeroToHero.kafka;
 
+import com.zeroToHero.entity.WikimediaData;
+import com.zeroToHero.repository.WikimediaDataRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -7,12 +9,22 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaDatabaseConsumer {
+
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaDatabaseConsumer.class);
+
+    private final WikimediaDataRepository dataRepository;
+    private final WikimediaData wikimediaData;
+
+    public KafkaDatabaseConsumer(WikimediaDataRepository dataRepository, WikimediaData wikimediaData) {
+        this.dataRepository = dataRepository;
+        this.wikimediaData = wikimediaData;
+    }
 
     //SpringFramework.kafka....
     @KafkaListener(topics = "wikimedia_recent_change",groupId = "myGroup")
     public void consume(String eventMessage){
         LOGGER.info(String.format("Event message received ->%s", eventMessage));
-
+        wikimediaData.setWikiEventData(eventMessage);
+        dataRepository.save(wikimediaData);
     }
 }
